ac7db179c979aafeb5f4bda823d57e53513cda8a
DDaxu <akselikoskela93@gmail.com>
2024-02-02 00:37:54 +0200

    bot functionality added, reading files and reacting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const {Client, IntentsBitField} = require('discord.js');
-require('dotenv').config();
+require('dotenv').config({path: '../.env'});
 const token = process.env.TOKEN;
+const fs = require('fs');
+const words = '../tulla.txt';
+var wordList = '';
 const client = new Client({
     intents: [
         IntentsBitField.Flags.Guilds,
@@ -12,6 +15,23 @@ const client = new Client({
 //main code here 
 client.on('ready', (c) => {
     console.log('bot is ready');
+    //reading file on start and converting it to string for later usage
+    var tempWords = fs.readFileSync(words);
+    wordList = tempWords.toString()
+        .split("\n")
+        .map(val => val.replace(/\r$/, ''));
+    console.log(wordList);
 });
-console.log(token);
+client.on('messageCreate', (msg) => {
+    var userMessage = msg.toString().toLowerCase();
+    var bool = false;
+    wordList.forEach(item => {
+        if(userMessage.includes(item)){
+            bool = true;
+        }
+    });
+    if(bool === true){
+        msg.react('💦');
+    }
+});
 client.login(token);
